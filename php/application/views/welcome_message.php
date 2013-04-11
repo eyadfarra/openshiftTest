a729e955c8e37f9997148c0a26d93bd78210fd34
eyadfarra <eyafa@live.com>
2013-04-11 18:51:41 +0300

    first commit
    the first commit

---
+++ php/application/views/welcome_message.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to my site</title>
+
+	
+</head>
+<body>
+	<h1>Hello every body</h1>
+	<p>Application owner: Eyad Mohammed Elfarra</p>
+	<h4>Hosted By: OpenShift</h4>
+	<dl>
+		<dt>OpenShift</dt>
+		<dd>OpenShift is Red Hat's free, auto-scaling Platform as a Service (PaaS) for applications. As an application platform in the cloud, OpenShift manages the stack so you can focus on your code.</dd>
+	</dl>
+</body>
+</html>
